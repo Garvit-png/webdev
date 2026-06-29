1f226b80c31e39652659afd8df5c699a1d04cbf3
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-06-29 12:23:19 +0530

    loops

--- Part1/4. Loops/script.js
+++ Part1/4. Loops/script.js
@@ -1,2 +1,41 @@
 // repeat krne ko loop kehte hai 
 
+// 1 - 100
+// for(let i=1 ; i<=100 ; i++){
+//     console.log(i);
+// }
+
+
+// let i = 1;
+// while(i <= 100){
+//     //code
+//     i++;
+// }
+
+
+// do - while loop -> mtlb ek bar toh pkka chalega 
+
+// let i = 12
+// do{
+//     console.log(i)
+//     i++
+
+// }
+// while(i<=10)
+
+
+
+
+
+
+for (let i = 1; i<102; i++){
+    console.log(i)
+    if (i==85){
+        break;
+    }
+}
+
+
+
+
+
